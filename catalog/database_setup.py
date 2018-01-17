289992da6fe3c9aef948f0e77060d30070d03072
Joseph Giampietro <joejkd82@gmail.com>
2018-01-16 22:28:26 -0500

    database_setup.py now at pep8 standard, version tested stable

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -16,59 +16,64 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Create Categories Table
+
+
 class Categories(Base):
-	__tablename__ = 'categories'
+    __tablename__ = 'categories'
 
-	name = Column(
-		String(80), nullable = False)
-	id = Column(
-		Integer, primary_key = True)
-	user_name = Column(String(250))
+    name = Column(
+        String(80), nullable=False)
+    id = Column(
+        Integer, primary_key=True)
+    user_name = Column(String(250))
 
-# Method to serialize data, can be used with JSONIFY to create JSON for each object
-	@property
-	def serialize(self):
-		return {
-		'name' : self.name,
-		'id' : self.id,
-		'user_name' : self.user_name
-		}
+# Method to serialize data, can be used with JSONIFY
+# to create JSON for each object
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            'user_name': self.user_name
+        }
 
 # Count of items for each Category row
-	@property
-	def getNumItems(self):
-		numItems = session.query(Items).filter_by(category_id=self.id).count()
-		return numItems
+    @property
+    def getNumItems(self):
+        numItems = session.query(Items).filter_by(category_id=self.id).count()
+        return numItems
 
 
 # Create items table
 class Items(Base):
-	__tablename__ = 'items'
-	
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	description = Column(String(250))
-	category_id = Column(Integer, ForeignKey('categories.id'))
-	categories = relationship(Categories)
-	user_name = Column(String(250))
+    __tablename__ = 'items'
 
-# Method to serialize data, can be used with JSONIFY to create JSON for each object
-	@property
-	def serialize(self):
-		return {
-		'name' : self.name,
-		'description' : self.description,
-		'id' : self.id,
-		'category_id' : self.category_id,
-		'user_name' : self.user_name
-		}
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
+    user_name = Column(String(250))
+
+# Method to serialize data, can be used with JSONIFY
+# to create JSON for each object
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'category_id': self.category_id,
+            'user_name': self.user_name
+        }
 
 # Method to allow front end dev get each items parent category from template
-	@property
-	def getCatName(self):
-		category = session.query(Categories).filter_by(id=self.category_id).one()
-		catname = category.name
-		return catname
+    @property
+    def getCatName(self):
+        category = session.query(Categories)\
+            .filter_by(id=self.category_id).one()
+        catname = category.name
+        return catname
 
 engine = create_engine('sqlite:///items.db')
 Base.metadata.create_all(engine)
